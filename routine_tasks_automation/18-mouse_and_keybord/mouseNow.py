9386fa6769a4dcf379e5750ca58cec7b64705d06
Valerii Stefanyk <valeriistefanyk@gmail.com>
2020-06-04 21:09:15 +0300

    Working with composit pattern and manage mouse kursor

---
+++ routine_tasks_automation/18-mouse_and_keybord/mouseNow.py
@@ -0,0 +1,20 @@
+"""
+mouseNow.py - Отбражает текущую позицию указателя мыши.
+"""
+
+import pyautogui
+
+print('Для выхода из программы - <Ctrl+C>.')
+try:
+    while True:
+        x, y = pyautogui.position()
+        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+
+        pixelColor = pyautogui.screenshot().getpixel((x, y))
+        positionStr += ' RGB: (' + str(pixelColor[0]).rjust(3)
+        positionStr += ', ' + str(pixelColor[1]).rjust(3)
+        positionStr += ', ' + str(pixelColor[2]).rjust(3) + ')'
+        print(positionStr, end='')
+        print('\b' * len(positionStr), end='', flush=True)
+except KeyboardInterrupt:
+    print('\nГотово.')
